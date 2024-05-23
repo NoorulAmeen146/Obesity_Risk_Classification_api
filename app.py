3e759a5046e92220ad16807ffc48493b2d82ad49
NoorulAmeen146 <noorulamean146@gmail.com>
2024-05-23 13:17:25 +0530

    experimental changes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import pickle
-import pandas as pd
 import subprocess
 import sys
 
@@ -8,13 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    subprocess.run([sys.executable,'-m','pip','install','pandas', 'numpy','--quiet'])
     subprocess.run([sys.executable,'-m','pip','install','scikit-learn','--quiet'])
     # return '<h1> Hello World, I\'m back! </h1>'
     return '<h1>Hello, This is an api for Obesity Risk Classification Inference!</h1>'
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
+    import pandas as pd
+    import sklearn
     with open('model.pkl','rb') as file:
         model = pickle.load(file)
     
