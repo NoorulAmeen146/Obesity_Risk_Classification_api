82ed4872a728f75bd5303411562c3ab414ae44f8
NoorulAmeen146 <noorulamean146@gmail.com>
2024-05-23 13:12:39 +0530

    change order of installing scikit-learn

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
-import importlib
 import subprocess
 import sys
 
@@ -9,12 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    subprocess.run([sys.executable,'-m','pip','install','scikit-learn','--quiet'])
     # return '<h1> Hello World, I\'m back! </h1>'
     return '<h1>Hello, This is an api for Obesity Risk Classification Inference!</h1>'
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    subprocess.run([sys.executable,'-m','pip','install','scikit-learn','--quiet'])
+    
     with open('model.pkl','rb') as file:
         model = pickle.load(file)
     
