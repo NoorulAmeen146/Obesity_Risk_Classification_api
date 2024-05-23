7e98c57b7a9be4c8223e3b0c2d42215b1fb208a0
NoorulAmeen146 <noorulamean146@gmail.com>
2024-05-23 11:19:03 +0530

    fix typo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return '<h1> Hello World, I\'m back! </h1>'
-    return '<h1>Hello, This is an api for Obesity Risk Classification Inference!<\h1>'
+    return '<h1>Hello, This is an api for Obesity Risk Classification Inference!</h1>'
 
 @app.route('/predict', methods=['POST'])
 def predict():
