bc3daf7ed88b669fab69e7a69f3b698762449b36
NoorulAmeen146 <noorulamean146@gmail.com>
2024-05-23 13:20:27 +0530

    experiment 2

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    subprocess.run([sys.executable,'-m','pip','install','pandas', 'numpy','--quiet'])
-    subprocess.run([sys.executable,'-m','pip','install','scikit-learn','--quiet'])
     # return '<h1> Hello World, I\'m back! </h1>'
     return '<h1>Hello, This is an api for Obesity Risk Classification Inference!</h1>'
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    subprocess.run([sys.executable,'-m','pip','install','pandas', 'numpy', 'scikit-learn','--quiet'])
+    # subprocess.run([sys.executable,'-m','pip','install','scikit-learn','--quiet'])
     import pandas as pd
     import sklearn
     with open('model.pkl','rb') as file:
