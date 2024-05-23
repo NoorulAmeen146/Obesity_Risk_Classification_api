6a470ffea804c34393c57d5ebf2686dd3f587030
NoorulAmeen146 <noorulamean146@gmail.com>
2024-05-23 10:56:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return '<h1> Hello World, I\'m back! </h1>'
+    return 'Hello, This is an api for Obesity Risk Classification Inference!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    with open('./source/model.pkl','rb') as file:
+        model = pickle.load(file)
+    
+    data = request.get_json(force=True)
+    
+    column_names = ['Age', 'Height', 'Weight', 'family_history_with_overweight', 'FAVC',
+       'FCVC', 'NCP', 'CAEC', 'SMOKE', 'CH2O', 'SCC', 'FAF', 'TUE', 'CALC',
+       'Gender_Female', 'Gender_Male']
+    
+    df = pd.DataFrame([data['feature']],columns=column_names)
+    prediction = model.predict(df)
+    target_col_map = {
+        0:'Insufficient_Weight',
+        1:'Normal_Weight',
+        2:'Overweight_Level_I',
+        3:'Overweight_Level_II',
+        4:'Obesity_Type_I',
+        5:'Obesity_Type_II',
+        6:'Obesity_Type_III'
+    }
+    output = {'prediction': target_col_map[int(prediction[0])]}
+
+    return jsonify(output)
